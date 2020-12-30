680c16bc18564cc674cb9a8e93696537088d9350
Moritz Frisch <mo68@posteo.net>
2020-12-30 19:36:42 +0100

    test

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
 #include "src/armstrong_numbers.h"
 #include <stdio.h>
 
-static int a[5];
-
-int main() {
-  a[5] = 0;
-  printf("%d is a Armstrong number", is_armstrong_number(6));
-}
+int main() { printf("%d is a Armstrong number", is_armstrong_number(6)); }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,9 @@
 #include "../src/armstrong_numbers.h"
 #include "gtest/gtest.h"
 
+TEST(Armstrong, 0) { EXPECT_TRUE(is_armstrong_number(0)); }
+TEST(Armstrong, 5) { EXPECT_TRUE(is_armstrong_number(5)); }
 TEST(Armstrong, 6) { EXPECT_TRUE(is_armstrong_number(6)); }
+TEST(Armstrong, 10) { EXPECT_FALSE(is_armstrong_number(10)); }
+TEST(Armstrong, 9926315) { EXPECT_TRUE(is_armstrong_number(9926315)); }
+TEST(Armstrong, 9926314) { EXPECT_FALSE(is_armstrong_number(9926314)); }
