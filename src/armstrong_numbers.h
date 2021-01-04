ffaf3b2aacec995e61636e4afe9fd9d7f52bd300
Moritz Frisch <mo68@posteo.net>
2021-01-04 17:55:24 +0100

    signed?

--- src/armstrong_numbers.h
+++ src/armstrong_numbers.h
@@ -5,15 +5,15 @@
 extern "C" {
 #endif
 
-#include <stdbool.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 unsigned int num_digits(unsigned int n);
 int num_digits2(int n);
 
 int ipow(int base, int exp);
 uint64_t ipow2(uint8_t base, uint8_t exp);
-bool is_armstrong_number(int candidate);
+bool is_armstrong_number(unsigned int candidate);
 bool is_armstrong_number_old(int candidate);
 
 #ifdef __cplusplus
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,6 @@
 #include "../src/armstrong_numbers.h"
 #include "gtest/gtest.h"
+#include <stdlib.h>
 
 TEST(Armstrong, 0) { EXPECT_TRUE(is_armstrong_number(0)); }
 TEST(Armstrong, 5) { EXPECT_TRUE(is_armstrong_number(5)); }
